71b5f26f0c624419d8026a742f3316e1a8ad455a
Anderson Luis Pinotti <anderson.pinotti@gmail.com>
2021-06-06 12:11:15 -0300

    primeiro commit do projeto

---
+++ app/Services/CriadorDeSerie.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Services;
+
+use App\{Serie, Temporada};
+use Illuminate\Support\Facades\DB;
+
+class CriadorDeSerie
+{
+
+    public function criarSerie (string $nomeSerie, int $qtdTemporadas, int $epPorTemporada) : Serie
+    {
+        DB::beginTransaction();
+        $serie = Serie::create(['nome' => $nomeSerie]);
+        $this->criaTemporadas($qtdTemporadas, $epPorTemporada, $serie);
+        DB::commit();
+
+        return $serie;
+    }
+
+    private function criaTemporadas(int $qtdTemporadas, int $epPorTemporada, Serie $serie) : void
+    {
+        for ($i = 1; $i <= $qtdTemporadas; $i++) {
+            $temporada = $serie->temporadas()->create(['numero' => $i]);
+            $this->criaEpisodios($epPorTemporada, $temporada);
+        }
+    }
+
+    private function criaEpisodios(int $epPorTemporada, Temporada $temporada) : void
+    {
+        for ($i = 1; $i <= $epPorTemporada; $i++) {
+            $temporada->episodios()->create(['numero' => $i]);
+        }
+    }
+}
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/7989db13d4.js" crossorigin="anonymous"></script>
+    <title>Controle de Séries</title>
+</head>
+<body>
+    <div class="container">
+
+        <div class="jumbotron">
+            <h1>@yield('cabecalho')</h1>
+        </div>
+
+        @yield('conteudo')
+        
+    </div>
+</body>
+</html>
